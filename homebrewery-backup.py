f066ddc24eeb2ac14e69f76d1952a528737906b6
Dekel <dekelpilli@gmail.com>
2017-12-13 11:19:09 +0200

    initial commit

---
+++ homebrewery-backup.py
@@ -0,0 +1,56 @@
+import urllib.request
+import sys
+import os
+import html2text
+from tqdm import tqdm
+import textwrap
+
+def getPages(user):
+    response = urllib.request.urlopen('http://homebrewery.naturalcrit.com/user/' + user)
+    html = response.read().decode('utf-8')
+
+    pages = []
+    if "No Brews." in html:
+        return pages
+
+    htmlSplit = html.split("/share/")[1:]
+    for p in htmlSplit:
+        pages.append(p.split('"')[0])
+
+    return pages
+
+def getSource(page):
+    response = urllib.request.urlopen('http://homebrewery.naturalcrit.com/source/' + page)
+    charset = response.headers.get_content_charset()
+    html = response.read()
+    html = html.decode(charset, errors='ignore')
+    
+    html2text.BODY_WIDTH = 0
+    md = html2text.html2text(html)[1:-3]
+    
+    
+    md = textwrap.dedent(md).strip()
+    
+    return md
+
+def writeMarkdownFiles(user, pages):
+    directory = os.getcwd()+"/"+user+"-brews/"
+    if not os.path.exists(directory):
+        os.makedirs(directory)
+    for page in tqdm(pages):
+        source = getSource(page)
+        f = open(directory+page+".md", 'w')
+        f.write(source.encode('ascii', 'ignore').decode('ascii')) #this will delete any unrecognisable characters
+        f.close()
+    
+if __name__ == "__main__":
+    if len(sys.argv) < 2:
+        print("Please enter your homebrewery username, like so:\n\thomebrewery-backup.py userNameHere")
+    else:
+        user = sys.argv[1]
+
+    pages = getPages(user)
+
+    writeMarkdownFiles(user, pages)
+    
+        
