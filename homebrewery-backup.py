15c24fa20dd378ef60114820b94746a095bd7aa5
Dekel Pilli <dekelpilli@gmail.com>
2017-12-19 03:27:13 +1100

    added brew names to markdown file names

--- homebrewery-backup.py
+++ homebrewery-backup.py
@@ -7,6 +7,29 @@ import textwrap
 import codecs
 import datetime
 
+class Brew:
+    def __init__(self, name=None, shareId=None):
+        self.name = name
+        self.shareId = shareId
+
+    def setShareId(self, shareId):
+        self.shareId = shareId
+
+    def setName(self, name):
+        self.name = name
+
+    def getName(self):
+        return self.name
+
+    def getShareId(self):
+        return self.shareId
+
+    def __str__(self):
+        return "Name: " + str(self.name) + "\nID: " + str(self.shareId) + "\n-------------------"
+
+    def getDescription(self):
+        return self.name + " - " + self.shareId 
+
 def getPages(user):
     response = urllib.request.urlopen('http://homebrewery.naturalcrit.com/user/' + user)
     html = response.read().decode('utf-8')
@@ -15,14 +38,26 @@ def getPages(user):
     if "No Brews." in html:
         return pages
 
-    htmlSplit = html.split("/share/")[1:]
-    for p in htmlSplit:
-        pages.append(p.split('"')[0])
+    htmlSplit = html.split('"brewItem" data-reactid="')[1:]
+    for brew in htmlSplit:
+        reactId = int(brew.split('"')[0])
+        name = brew.split('<h2 data-reactid="'+str(reactId+1)+'">')[1].split('</h2>')[0]
+        shareId = brew.split("/share/")[1:][0].split('"')[0]
+
+        brewItem = Brew(name, shareId)
+        pages.append(brewItem)
+
+    for p in pages:
+        print(p)
+    
+##    htmlSplit = html.split("/share/")[1:]
+##    for p in htmlSplit:
+##        pages.append(p.split('"')[0])
 
     return pages
 
-def getSource(page):
-    response = urllib.request.urlopen('http://homebrewery.naturalcrit.com/source/' + page)
+def getSource(brew):
+    response = urllib.request.urlopen('http://homebrewery.naturalcrit.com/source/' + brew.getShareId())
     charset = response.headers.get_content_charset()
     html = response.read()
     html = html.decode(charset, errors='ignore')
@@ -35,7 +70,7 @@ def getSource(page):
     
     return md
 
-def writeMarkdownFiles(user, pages):
+def writeMarkdownFiles(user, brews):
     baseDir = os.getcwd()+"/backups/"+user+datetime.datetime.now().strftime('-%d.%m.%y_%H-%M-%S')
     directory = baseDir + "/"
     copy = 1
@@ -43,17 +78,18 @@ def writeMarkdownFiles(user, pages):
         directory = baseDir + "(Copy " + str(copy) + ")/"
         copy += 1
     os.makedirs(directory)
-    for page in tqdm(pages):
-        source = getSource(page)
-        f = codecs.open(directory+page+".md", 'w', 'utf-8')
+    for brew in tqdm(brews):
+        source = getSource(brew)
+        f = codecs.open(directory+brew.getDescription()+".md", 'w', 'utf-8')
         f.write(source) 
         f.close()
     
 if __name__ == "__main__":
     if len(sys.argv) < 2:
         print("Please enter your homebrewery username, like so:\n\thomebrewery-backup.py userNameHere")
+        exit(1)
     else:
-        user = sys.argv[1]
+        user = str(sys.argv[1])
 
     pages = getPages(user)
 
