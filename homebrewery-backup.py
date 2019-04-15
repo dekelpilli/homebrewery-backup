904d087b9fe90effbcbeb177822615d6dcdb76af
Dekel Pilli <dekelpilli@gmail.com>
2019-04-15 21:12:12 +1000

    Changed date format for sorting purposes

--- homebrewery-backup.py
+++ homebrewery-backup.py
@@ -68,7 +68,7 @@ def get_source(brew):
 
 def write_markdown_files(user, brews):
     # folder name based on user name and date
-    base_dir = os.getcwd() + "/backups/" + user + datetime.now().strftime('-%d.%m.%y_%H-%M-%S')
+    base_dir = os.getcwd() + "/backups/" + user + datetime.now().strftime('-%y.%m.%d_%H-%M-%S')
     directory = base_dir + "/"
     copy = 1
     while os.path.exists(directory):  # make 100% sure folder doesn't overwrite current files
